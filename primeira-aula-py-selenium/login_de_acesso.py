7d52d1c480954d0ac790f3b9b4cd28f083031d30
Marcos <quinhomarcosjr@gmail.com>
2024-11-15 07:32:05 -0300

    first commit

---
+++ primeira-aula-py-selenium/login_de_acesso.py
@@ -0,0 +1,11 @@
+usuario = input("Qual é seu usuario programador? ")
+senha_cadastro = 55631475
+senha = int(input("Senha de acesso Programador!! "))
+if usuario == "Marcos" and senha == senha_cadastro:
+    print('Acesso liberado')
+    
+else:
+    print("""Acesso negado!!
+###############################           
+Não conhecemos esse usuario.""")
+    
---
+++ primeira-aula-py-selenium/primeira-aula.py
@@ -0,0 +1,9 @@
+nome = "Marcos"
+idade = 32
+sexo = "M"
+consulta = nome + sexo
+print("Olá curso de python com selenium")
+print(type(True))
+print(type(12))
+print(type(consulta))
+print("Olá", consulta,(type(sexo)))
